d7e949e0f744c6d95572aeecece9011a11c408a5
jsara <jadwiga.sarata@gmail.com>
2022-10-24 14:16:06 +0200

    create serializer and viewset for genre

--- book_app/genres/tests.py
+++ book_app/genres/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
 
-# Create your tests here.
+from genres.serializers import GenreSerializer
+from genres.models import Genre
+
+
+GENRES_URL = reverse('genre:genre-list')
+
+
+class GenreApiTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_get_genres_with_no_genres(self):
+        resp = self.client.get(GENRES_URL)
+        expected = []
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(resp.json(), expected)
+
+    def test_get_genres_with_two_genres(self):
+        Genre.objects.create(name='Drama')
+        Genre.objects.create(name='Crime')
+
+        resp = self.client.get(GENRES_URL)
+
+        genres = Genre.objects.all().order_by('name')
+        serializer = GenreSerializer(genres, many=True)
+
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(resp.data, serializer.data)
+
+    def test_post_genre_nonexistent(self):
+        new_genre_name = 'Drama'
+        payload = {'name': new_genre_name}
+        resp = self.client.post(GENRES_URL, payload)
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(resp.data['name'], new_genre_name)
+
+    def test_post_genre_existing(self):
+        new_genre_name = 'History'
+        Genre.objects.create(name=new_genre_name)
+        payload = {'name': new_genre_name}
+        resp = self.client.post(GENRES_URL, payload)
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
--- book_app/genres/views.py
+++ book_app/genres/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from genres.models import Genre
+from genres.serializers import GenreSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows to view or modify genres.
+    """
+    queryset = Genre.objects.all().order_by('name')
+    serializer_class = GenreSerializer
