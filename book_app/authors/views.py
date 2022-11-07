975410652a20fa1c80d70dc39cfa8c6ed4209531
jsara <jadwiga.sarata@gmail.com>
2022-11-07 14:36:12 +0100

    add authors viewset

--- book_app/authors/views.py
+++ book_app/authors/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from authors.models import Author
+from authors.serializers import AuthorSerializer
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allow to view or modify genre.
+    """
+    queryset = Author.objects.all().order_by('last_name', 'first_name')
+    serializer_class = AuthorSerializer
