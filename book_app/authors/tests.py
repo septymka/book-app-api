80017ceb65a74d5a54a39d1d52f77c5a2c8671ea
jsara <jadwiga.sarata@gmail.com>
2022-11-09 14:34:24 +0100

    move detail_url to test class

--- book_app/authors/tests.py
+++ book_app/authors/tests.py
@@ -13,10 +13,6 @@ from authors.factories import AuthorFactory
 AUTHORS_URL = reverse('author:author-list')
 
 
-def detail_url(author_id):
-    return reverse('author:author-detail', args=[author_id])
-
-
 class AuthorApiTests(TestCase):
 
     def setUp(self):
@@ -54,6 +50,9 @@ class AuthorApiTests(TestCase):
 
 class DetailAuthorApiTests(TestCase):
 
+    def detail_url(self, author_id):
+        return reverse('author:author-detail', args=[author_id])
+
     def setUp(self):
         self.client = APIClient()
         self.author = AuthorFactory()
@@ -64,34 +63,34 @@ class DetailAuthorApiTests(TestCase):
 
     def test_get_author_detail_existing(self):
         serializer = AuthorSerializer(self.author)
-        url = detail_url(self.author.id)
+        url = self.detail_url(self.author.id)
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertEqual(resp.data, serializer.data)
 
     def test_get_author_detail_nonexistent(self):
-        url = detail_url(self.nonexistent_author_id)
+        url = self.detail_url(self.nonexistent_author_id)
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_patch_author(self):
-        url = detail_url(self.author.id)
+        url = self.detail_url(self.author.id)
         resp = self.client.patch(url, self.payload)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         self.assertEqual(resp.data['first_name'], self.payload['first_name'])
 
     def test_patch_author_nonexistent(self):
-        url = detail_url(self.nonexistent_author_id)
+        url = self.detail_url(self.nonexistent_author_id)
         resp = self.client.patch(url, self.payload)
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_delete_author(self):
-        url = detail_url(self.author.id)
+        url = self.detail_url(self.author.id)
         resp = self.client.delete(url)
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Author.objects.filter(id=self.author.id).exists())
 
     def test_delete_author_nonexistent(self):
-        url = detail_url(self.nonexistent_author_id)
+        url = self.detail_url(self.nonexistent_author_id)
         resp = self.client.delete(url)
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
