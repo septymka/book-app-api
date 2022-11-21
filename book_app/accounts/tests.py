9beabda9e2a4c2f3534c040a6816f11d71bfcf27
jsara <jadwiga.sarata@gmail.com>
2022-11-21 15:17:24 +0100

    add viewset for custom user account and token create

--- book_app/accounts/tests.py
+++ book_app/accounts/tests.py
@@ -1,3 +1,62 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from accounts import factories
+
+
+class UserApiTest(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.create_user_url = reverse('account:create')
+        self.token_url = reverse('account:token')
+        self.payload_user = {
+            'email': 'test@example.com',
+            'password': 'test123',
+            'first_name': 'First',
+            'last_name': 'Last'
+        }
+        self.payload_token = {
+            'email': 'test@example.com',
+            'password': 'test123',
+        }
+
+    def test_create_user_success(self):
+        resp = self.client.post(self.create_user_url, self.payload_user)
+
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        user = get_user_model().objects.get(email=self.payload_user['email'])
+        self.assertTrue(user.check_password(self.payload_user['password']))
+        self.assertNotIn('password', resp.data)
+
+    def test_user_with_existing_email(self):
+        user = factories.UserFactory(**self.payload_user)
+        resp = self.client.post(self.create_user_url, self.payload_user)
+
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_shot(self):
+        self.payload_user['password'] = 'test'
+        resp = self.client.post(self.create_user_url, self.payload_user)
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(email=self.payload_user['email']).exists()
+        self.assertFalse(user_exists)
+
+    def test_create_token(self):
+        user = factories.UserFactory(**self.payload_user)
+        resp = self.client.post(self.token_url, self.payload_token)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertIn('token', resp.data)
+
+
+    def test_create_token_bad_password(self):
+        user = factories.UserFactory(**self.payload_user)
+        self.payload_token['password'] = 'incorrect'
+        resp = self.client.post(self.token_url, self.payload_token)
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', resp.data)
--- book_app/accounts/views.py
+++ book_app/accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-# Create your views here.
+from accounts.serializers import (
+    UserSerializer,
+    AuthTokenSerializer
+)
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
