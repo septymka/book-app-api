ba408a73cf2c4f86cd886a24138ea15b9dea5618
jsara <jadwiga.sarata@gmail.com>
2022-11-22 09:34:42 +0100

    add authentiaction required to post reviews

---
+++ book_app/reviews/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from reviews import views
+
+
+router = DefaultRouter()
+router.register('reviews', views.BookReviewViewSet)
+
+app_name = 'review'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
--- book_app/reviews/views.py
+++ book_app/reviews/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
 
 from reviews.models import BookReview
 from reviews.serializers import BookReviewSerializer
@@ -10,3 +12,11 @@ class BookReviewViewSet(viewsets.ModelViewSet):
     """
     queryset = BookReview.objects.all()
     serializer_class = BookReviewSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
+
+    def create(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            super().create()
+        else:
+            data = {'message': 'Authentication required'}
+            return Response(data, status=status.HTTP_401_UNAUTHORIZED)
